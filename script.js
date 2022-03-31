78701dd891821f08a617b04526b90931f9ac55f6
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-31 15:44:08 -0300

    feito - requisito 08

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttonLogin = document.querySelector('.btn-login');
-const emailInput = document.querySelector('.email-input');
-const passwordInput = document.querySelector('.password-input');
+const emailInput = document.querySelector('.email-login-input');
+const passwordInput = document.querySelector('.password-login-input');
 
 buttonLogin.addEventListener('click', () => {
   if (emailInput.value === 'tryber@teste.com' && passwordInput.value === '123456') {
