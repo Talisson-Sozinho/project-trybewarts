3ee9350c2eea842dbc3dbf8bd3b03a2d6b80e5f4
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-31 19:16:38 -0300

    feito - requisito 21

--- script.js
+++ script.js
@@ -21,3 +21,53 @@ agreementCheckbox.addEventListener('change', (event) => {
 textareaInput.addEventListener('input', () => {
   counterLenthTextArea.innerText = 500 - textareaInput.value.length;
 });
+
+function createTextElement(tag, text) {
+  const element = document.createElement(tag);
+  element.innerText = text;
+  return element;
+}
+
+function createCheckboxText() {
+  const checkboxSelectedArray = document.querySelectorAll('input[name="content"]:checked');
+  const selectedArray = [];
+
+  for (let index = 0; index < checkboxSelectedArray.length; index += 1) {
+    selectedArray.push(checkboxSelectedArray[index].value);
+  }
+
+  return selectedArray.join(', ');
+}
+
+function createObjectOfForm() {
+  const firstName = document.getElementById('input-name').value;
+  const lastName = document.getElementById('input-lastname').value;
+  const fullName = `${firstName} ${lastName}`;
+
+  const formObj = {
+    fullName,
+    email: document.getElementById('input-email').value,
+    house: document.getElementById('house').value,
+    family: document.querySelector('input[name="family"]:checked').value,
+    checkbox: createCheckboxText(),
+    avaliationRate: document.querySelector('input[name="rate"]:checked').value,
+    textareaText: document.getElementById('textarea').value,
+  };
+  return formObj;
+}
+
+formSubmitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const form = document.getElementById('evaluation-form');
+  const formObj = createObjectOfForm();
+
+  form.innerHTML = null;
+
+  form.appendChild(createTextElement('p', `Nome: ${formObj.fullName}`));
+  form.appendChild(createTextElement('p', `Email: ${formObj.email}`));
+  form.appendChild(createTextElement('p', `Casa: ${formObj.house}`));
+  form.appendChild(createTextElement('p', `Família: ${formObj.family}`));
+  form.appendChild(createTextElement('p', `Matérias: ${formObj.checkbox}`));
+  form.appendChild(createTextElement('p', `Avaliação: ${formObj.avaliationRate}`));
+  form.appendChild(createTextElement('p', `Observações: ${formObj.textareaText}`));
+});
